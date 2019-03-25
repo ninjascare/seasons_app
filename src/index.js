8c9321676c81a5e959c2ecd41f61a6e9fe022ce5
ninjascare <you@example.com>
2019-03-25 15:04:10 -0400

    added more refactoring

--- src/index.js
+++ src/index.js
@@ -21,15 +21,18 @@ class App extends Component {
     );
   }
 
-  render() {
+  renderContent() {
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error:{this.state.errorMessage}</div>;
     }
     if (!this.state.errorMessage && this.state.lat) {
       return <SeasonDisplay lat={this.state.lat} />;
     }
-
-    return <Spinner />;
+    return <Spinner message="Please accept location request" />;
+  }
+  
+  render() {
+    return <div className="border red">{this.renderContent()}</div>;
   }
 }
 
