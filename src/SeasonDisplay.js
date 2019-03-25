2bce07ccec2198823e24651defa8f403102e748d
ninjascare <you@example.com>
2019-03-25 11:31:49 -0400

    working on getting my second component to displsy

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,7 +1,16 @@
 import React from "react";
 
-const SeasonDisplay = () => {
-  return <div>Seadon Display</div>;
+const determineSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? "summer" : "winter";
+  } else {
+    return lat > 0 ? "winter" : "summer";
+  }
+};
+
+const SeasonDisplay = props => {
+  const season = determineSeason(props.lat, new Date().getMonth());
+  return <div>{}</div>;
 };
 
 export default SeasonDisplay;
